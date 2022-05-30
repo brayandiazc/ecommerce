c90c7c0a9bf2a90a94e44a76241516898166748d
Brayan Diaz C <brayandiazc@gmail.com>
2022-05-30 18:26:15 -0500

    feat: Join to product_category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -58,13 +58,14 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def product_params
-      params.require(:product).permit(:name, :description, :stock, :price, :sku)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def product_params
+    params.require(:product).permit(:name, :description, :stock, :price, :sku, { category_ids: [] })
+  end
 end
