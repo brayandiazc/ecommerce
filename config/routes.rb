38a29f9ff5cb9d4ef5f098d449e359952777a3ae
Brayan Diaz C <brayandiazc@gmail.com>
2022-05-31 20:21:40 -0500

    feat: User model with devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admins
   get "home/index"
   root to: "home#index"
-  resources :products
-  resources :categories
+  authenticate :admin do
+    resources :products
+    resources :categories
+  end
 end
