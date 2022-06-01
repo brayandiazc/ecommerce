0edf347a7109e25ea840f60005b2d14df359875c
Brayan Diaz C <brayandiazc@gmail.com>
2022-06-01 17:35:08 -0500

    feat: Test red ok

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "creates a random number on create" do
+    user = User.create(email: "user@example.com", password: "12345678")
+    order = Order.create(user_id: user.id)
+    assert !order.number.nil?
+  end
 end
