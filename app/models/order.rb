588e1848e76da6a5dd8609898d8b96ddb17a30ee
Brayan Diaz C <brayandiazc@gmail.com>
2022-06-01 17:50:48 -0500

    feat: Test green ok

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,26 @@
 class Order < ApplicationRecord
-  before_create :generate_number
+  before_create -> { generate_number(hash_size) }
   belongs_to :user
 
-  def generate_number
+  # Validations
+  validates :number, uniqueness: true
+
+  def generate_number(size)
     self.number ||= loop do
-      random = "BO#{Array.new(9) { rand(9) }.join}"
+      random = random_candidate(size)
       break random unless self.class.exists?(number: random)
     end
+  end
+
+  def random_candidate(size)
+    "#{hash_prefix}#{Array.new(size) { rand(size) }.join}"
+  end
+
+  def hash_prefix
+    "BO"
+  end
+
+  def hash_size
+    9
   end
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -6,4 +6,11 @@ class OrderTest < ActiveSupport::TestCase
     order = Order.create(user_id: user.id)
     assert !order.number.nil?
   end
+
+  test "number must be unique" do
+    user = User.create(email: "user@example.com", password: "12345678")
+    order = Order.create(user_id: user.id)
+    duplicated_order = order.dup
+    assert_not duplicated_order.valid?
+  end
 end
